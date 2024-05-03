1b46db427addada41c4fe908ef7053d6389bc7af
skyler marquis <skylerjmarquis@gmail.com>
2024-05-02 22:17:15 -0500

    Added some input validation to reminders. Added more fields on checklist create view

--- PlannerApplication/Models/Reminders.cs
+++ PlannerApplication/Models/Reminders.cs
@@ -6,9 +6,12 @@ namespace PlannerApplication.Models
     {
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Please enter a reminder title")]
+        [StringLength(20, ErrorMessage ="Title must be 20 characters or less.")]
         public string Title { get; set; }
 
+        [Required(ErrorMessage = "Please enter a reminder description")]
+        [StringLength(100, ErrorMessage ="Description must be 100 characters or less.")]
         public string Description { get; set; }
 
         [DataType(DataType.DateTime)]
